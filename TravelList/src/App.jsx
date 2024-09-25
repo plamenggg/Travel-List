3bb9d2c81a9f44185d16e2391dd4f62e1ea27dca
plamenggg <plamen_ganchev_94@abv.bg>
2024-09-25 15:50:35 +0300

    Implemented form functionality

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 function App() {
@@ -22,10 +25,41 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -44,9 +78,9 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
-        </span>
+      </span>
       <button>❌</button>
     </li>
   );
