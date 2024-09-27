014d75810a5370a0aa3a73c69d2d732c15cec79f
plamenggg <plamen_ganchev_94@abv.bg>
2024-09-27 13:05:00 +0300

    Implemented clear list functionality

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -20,6 +20,13 @@ function App() {
     );
   }
 
+  function handleClearList() {
+
+    const confirmed = window.confirm('Are you sure you want to delete all items?');
+
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -28,6 +35,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -80,7 +88,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -115,6 +123,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description"> Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear List</button>
       </div>
     </div>
   );
