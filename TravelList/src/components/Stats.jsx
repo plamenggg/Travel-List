d1959203fe1da3e8c61c22b4e317c390aa58deba
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-08 15:43:52 +0300

    Small change in the App.jsx

--- TravelList/src/components/Stats.jsx
+++ TravelList/src/components/Stats.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-export function Stats({ items }) {
+export default function Stats({ items }) {
   if (!items.length)
     return (
       <p className="stats">
